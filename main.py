57816ca94801d9c081467e836c9f4cbbb611e419
Helder Gonçalves <helder.goncalves@spideronline.co.uk>
2024-10-27 20:11:31 +0000

    feat: movement

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        dt = time.tick(60) / 1000
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 if __name__ == "__main__":
   main()
